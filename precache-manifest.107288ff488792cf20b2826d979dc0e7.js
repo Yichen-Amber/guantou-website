63fa985a8468363b6adfe900cf7f5022cce363e5
Yichen <2283815345@qq.com>
2021-09-16 16:43:38 +0800

    deploy

---
+++ precache-manifest.107288ff488792cf20b2826d979dc0e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d130851c6baba5baa09",
+    "url": "/guantou-website/css/app.8d51810d.css"
+  },
+  {
+    "revision": "ccf46f94436cc41513733ceafe8ee9db",
+    "url": "/guantou-website/index.html"
+  },
+  {
+    "revision": "5d130851c6baba5baa09",
+    "url": "/guantou-website/js/app.649a44c7.js"
+  },
+  {
+    "revision": "3e97133784111d8e9bc5",
+    "url": "/guantou-website/js/chunk-vendors.a45abbff.js"
+  },
+  {
+    "revision": "778e6a98c0fed1bbcd471f79a4bc341a",
+    "url": "/guantou-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/guantou-website/robots.txt"
+  }
+]);
